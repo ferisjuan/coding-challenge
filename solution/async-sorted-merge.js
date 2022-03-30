d7885df49d834823f3407ea094a5a3f9db1028d7
Juan Feris <feris.juan@gmail.com>
2022-03-30 08:43:33 -0500

    resolve challenge 2

--- solution/async-sorted-merge.js
+++ solution/async-sorted-merge.js
@@ -2,8 +2,25 @@
 
 // Print all entries, across all of the *async* sources, in chronological order.
 
+const mergeSort = require("./merge-sort");
+const parsedLogs = require("./log-parsers");
+
+const resolver = (logSources, printer) => {
+      const sortedLogs = mergeSort(logSources);
+      const logs = parsedLogs(sortedLogs);
+
+      logs.forEach(log => {
+        printer.print(log);
+      })
+
+      printer.done();
+
+
+      console.log("Async sort complete.")
+    }
+
 module.exports = (logSources, printer) => {
   return new Promise((resolve, reject) => {
-    resolve(console.log("Async sort complete."));
+    resolve(resolver(logSources, printer));
   });
 };
--- solution/sync-sorted-merge.js
+++ solution/sync-sorted-merge.js
@@ -1,9 +1,9 @@
 "use strict";
 
+// Print all entries, across all of the sources, in chronological order.
+
 const mergeSort = require("./merge-sort");
 const parsedLogs = require("./log-parsers");
-
-// Print all entries, across all of the sources, in chronological order.
 
 module.exports = (logSources, printer) => {
   const sortedLogs = mergeSort(logSources);
